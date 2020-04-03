07834541b912485a3ceb8013b36608dd094440b3
julianmatthewman <julianmatthewman@gmail.com>
2020-04-03 15:20:28 +0100

    moved app to folder

--- COVID_Dashboard/app.R
+++ COVID_Dashboard/app.R
@@ -1,11 +1,21 @@
 library(shiny)
 library(shinydashboard)
-
-
-## Data wrangling
 library(tidyverse)
 library(lubridate)
+
+
+##############################
+# Data Import
+##############################
+
 ts_conf <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", stringsAsFactors = F))
+
+
+
+##############################
+# Data wrangling
+##############################
+
 #Renaming the "Province/State" and "Country.Region" columns to State and Country respectively
 colnames(ts_conf)[1] <- "State"
 colnames(ts_conf)[2] <- "Country"
@@ -23,6 +33,10 @@ high <- unique(ts_conf_gathered$Country[ts_conf_gathered$cases>8000])
 
 
 
+##############################
+#UI
+##############################
+
 
 ui <- dashboardPage(
     dashboardHeader(title = "COVID-19 Dashboard"),
@@ -32,7 +46,7 @@ ui <- dashboardPage(
     )),
     dashboardBody(
         tabItems(
-            ### First tab content
+            ### First tab content (Plot)
             tabItem(tabName = "confirmed", #the tab name needs to be one word
                     fluidRow(
                         box(width = 9, plotOutput("plot")),
@@ -42,22 +56,39 @@ ui <- dashboardPage(
                             )
                         )
                     ),
-            ### Second Tab content
+            ### Second Tab content (Map)
             tabItem(tabName = "map",
-                    h2("Map of Cases"))
+                    fluidPage(
+                        leafletOutput("map", height = 500)
+                        ))
             )
         )
     )
 
+
+
+##############################
+#Server
+##############################
+
 server <- function(input, output) {
-    output$plot <- renderPlot({
-        ## Plot displaying cumulative confirmed COVID cases by country
+    output$plot <- renderPlot({ ### Plot displaying cumulative confirmed COVID cases by country
         mygg <-  ggplot() +
             geom_line(data=filter(ts_conf_gathered, Country %in% input$country), aes(x=date, y=cases, group=Country, colour=Country)) +
             labs(title = "Confirmed COVID19 cases by country")
         #add log scale if log is selected in the Scale input
         ifelse(input$scale == "log", return(mygg + scale_y_log10()), return(mygg))
     })
+    
+    
+    output$map <- renderLeaflet({ ### Map displaying COVID cases
+        leaflet(ts_conf) %>% 
+            addTiles() %>% 
+            addCircleMarkers(radius = ts_conf$`4/2/20`/4000,
+                             stroke = FALSE,
+                             color = "red")
+    })
+    
 }
 
 shinyApp(ui, server)
